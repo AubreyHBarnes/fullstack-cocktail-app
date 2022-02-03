30529522c8fe4567ada63002cba220361e7896d0
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2022-02-03 11:21:07 -0600

    adding netlify functions

---
+++ netlify/functions/fetch-ex.js
@@ -0,0 +1,12 @@
+exports.handler = async function () {
+
+    const cocktailAPI = `https://www.themealdb.com/api/json/v2/${process.env.COCKTAIL_SECRET}/latest.php`
+    
+    const res = await fetch(cocktailAPI);
+    const data = await res.json();
+
+    return {
+        statusCode: 200, 
+        body: JSON.stringify(data)
+    }
+}
